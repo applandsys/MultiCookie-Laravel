e9681a1d186eff7dc92b2540ff1e26013b7807f0
Tarique Mosharraf <applandsys@gmail.com>
2023-05-29 15:31:43 +0600

    migration proxy site

---
+++ app/Models/Proxy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proxy extends Model
+{
+    use HasFactory;
+    protected $fillable = ['type','ip','port','username','password','status','expiry_date'];
+}
---
+++ database/migrations/2023_05_29_071850_create_proxies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proxies', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('ip');
+            $table->string('port');
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->string('status')->nullable();
+            $table->string('expiry_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proxies');
+    }
+};
